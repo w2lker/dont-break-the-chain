c4a3732080f91b3117477cf3731b1d617508255f
Roman Bova <roman.bova@wallester.eu>
2019-11-19 20:00:04 +0200

    Add styleUtils boxEdges function

--- src/utils/styleUtils/helpers/helpers.test.ts
+++ src/utils/styleUtils/helpers/helpers.test.ts
@@ -1,37 +1,145 @@
 import cases from 'jest-in-case';
 
-import { cleanMap, isParamUndefined } from './helpers';
+import { boxEdges, cleanMap, isParamUndefined } from './helpers';
 
-describe('Test style helpers', () => {
-  describe('isParamUndefined', () => {
-    cases('isParamUndefined cases', (opts) => {
-      expect(isParamUndefined(opts.param)).toBe(opts.response);
-    }, [
-      { name: 'Null is true', param: null, response: true },
-      { name: 'Undefined is true', param: undefined, response: true },
-      { name: '"" is true', param: '', response: true },
-      { name: 'unset is true', param: 'unset', response: true },
-      { name: '0 is false', param: 0, response: false },
-    ]);
+describe('isParamUndefined', () => {
+  const casesTitle = 'isParamUndefined cases';
+  const casesTester = (opts: typeof casesData[1]) => {
+    expect(isParamUndefined(opts.param)).toBe(opts.response);
+  };
+  const casesData = [
+    {
+      name: 'Null is true',
+      param: null,
+      response: true,
+    },
+    {
+      name: 'Undefined is true',
+      param: undefined,
+      response: true,
+    },
+    {
+      name: '"" is true',
+      param: '',
+      response: true,
+    },
+    {
+      name: 'unset is true',
+      param: 'unset',
+      response: true,
+    },
+    {
+      name: '"null" is true',
+      param: 'null',
+      response: true,
+    },
+    {
+      name: '0 is false',
+      param: 0,
+      response: false,
+    },
+  ];
+  cases(casesTitle, casesTester, casesData);
+});
+
+describe('cleanMap', () => {
+  const sampleDataToSave = {
+    a: 1,
+    a0: 0,
+    ['undefined']: 23,
+  };
+  const sampleData = {
+    ...sampleDataToSave,
+    c: null,
+    unset: 'unset',
+    nulled: 'null',
+  };
+
+  const casesTitle = 'cleanMap cases';
+  const casesTester = (opts: typeof casesData[1]) => {
+    // @ts-ignore
+    expect(cleanMap(opts.input)).toEqual(opts.output);
+  };
+  const casesData = [
+    {
+      name: 'Null to empty object',
+      input: null,
+      output: {},
+    },
+    {
+      name: 'Unset to empty object',
+      input: { some: 'unset' },
+      output: {},
+    },
+    {
+      name: 'String "null" to empty object',
+      input: { some: 'null' },
+      output: {},
+    },
+    {
+      name: 'SampleData objects',
+      input: sampleData,
+      output: sampleDataToSave,
+    },
+  ];
+  cases(casesTitle, casesTester, casesData);
+});
+
+describe('boxEdges', () => {
+  beforeEach(() => {
+    // tslint:disable-next-line:no-empty
+    jest.spyOn(console, 'error').mockImplementation(() => {});
   });
-  describe('cleanMap', () => {
-    const sampleDataToSave = {
-      a: 1,
-      a0: 0,
-      ['undefined']: 23,
-    };
-    const sampleData = {
-      ...sampleDataToSave,
-      c: null,
-      unset: 'unset',
-    };
-    cases('cleanMap cases', (opts) => {
-      // @ts-ignore
-      expect(cleanMap(opts.input)).toEqual(opts.output);
-    }, [
-      { name: 'Null to empty object', input: null, output: {} },
-      { name: 'Unset to empty object', input: { some: 'unset' }, output: {} },
-      { name: 'SampleData objects', input: sampleData, output: sampleDataToSave },
-    ]);
+  afterEach(() => {
+    // @ts-ignore
+    // tslint:disable-next-line:no-console
+    console.error.mockRestore();
   });
+
+  const casesTitle = 'boxEdges cases';
+  const casesTester = (opts: typeof casesData[1]) => {
+    expect(boxEdges(opts.input)).toEqual(opts.output);
+    // tslint:disable-next-line:no-console
+    expect(console.error).toHaveBeenCalledTimes(opts.mocks);
+  };
+
+  const casesData = [
+    {
+      name: 'For null should be null',
+      input: null,
+      output: null,
+      mocks: 0,
+    },
+    {
+      name: 'For 1 param',
+      input: '14px',
+      output: ['14px', '14px', '14px', '14px'],
+      mocks: 0,
+    },
+    {
+      name: 'For 2 params',
+      input: '14px 45px',
+      output: ['14px', '45px', '14px', '45px'],
+      mocks: 0,
+    },
+    {
+      name: 'For 3 params',
+      input: '14px 45px 2em',
+      output: ['14px', '45px', '2em', '14px'],
+      mocks: 0,
+    },
+    {
+      name: 'For 4 params',
+      input: '14px 45px 2em 5vw',
+      output: ['14px', '45px', '2em', '5vw'],
+      mocks: 0,
+    },
+    {
+      name: 'For 5 params',
+      input: '14px 45px 2em 5vw 54px',
+      output: null,
+      mocks: 1,
+    },
+  ];
+  cases(casesTitle, casesTester, casesData);
 });
--- src/utils/styleUtils/helpers/helpers.ts
+++ src/utils/styleUtils/helpers/helpers.ts
@@ -1,5 +1,5 @@
 export function isParamUndefined(param: any): boolean {
-  return (!param || param === 'unset') && param !== 0;
+  return (!param || param === 'unset' || param === 'null') && param !== 0;
 }
 
 export function cleanMap(sourceMap: {}): {} {
@@ -11,10 +11,47 @@ export function cleanMap(sourceMap: {}): {} {
   };
   for (const propName in resultMap) {
     // @ts-ignore
-    if (resultMap[propName] === null || resultMap[propName] === undefined || resultMap[propName] === 'unset') {
+    if (resultMap[propName] === null || resultMap[propName] === undefined || resultMap[propName] === 'unset' || resultMap[propName] === 'null') {
       // @ts-ignore
       delete resultMap[propName];
     }
   }
   return resultMap;
+}
+
+export function boxEdges(composed: string | null): string[] | null {
+  if (!composed) {
+    return null;
+  }
+  // in case long string has been provided take reasonable part of it
+  const boundes = composed.substr(0, 1000).trim().replace(/\s{2,}/g, ' ');
+  const split = boundes.split(' ');
+  if (split.length > 4) {
+    // tslint:disable-next-line:no-console
+    console.error(`Params error: expected to receive from 1 to 4 entries, but found ${split.length} entries. Please check input data`);
+    return null;
+  }
+  switch (split.length) {
+    case 4:
+      return split;
+    case 3:
+      return split.concat(split[0]);
+    case 2:
+      return split.concat(split);
+    case 1:
+      return Array(4).fill(split[0]);
+  }
+  return [];
+}
+
+export function reduceBoundedEdges(keys: string[] | null, values: string[] | null) {
+  if (!keys || !values) {
+    return {};
+  }
+  // TODO: fallback error - values.length !== keys.length
+  return keys.reduce((result, keyName, index) => {
+      // @ts-ignore
+    result[keyName] = values[index];
+    return result;
+  }, {});
 }
