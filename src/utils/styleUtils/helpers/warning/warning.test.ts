4f0075fd0616c344abe5e23d4e1268dc52643b69
Roman <romanbova@gmail.com>
2019-12-08 15:11:53 +0200

    Add initial layout for bon-jss warning function

---
+++ src/utils/styleUtils/helpers/warning/warning.test.ts
@@ -0,0 +1,6 @@
+describe('bonWarning', () => {
+  // TODO: mock console.warn with jest spy
+  test.todo('it works regular way');
+  test.todo('it throws error if there is no argument');
+  test.todo('it skips sending console.warn if console is not defined');
+});
---
+++ src/utils/styleUtils/helpers/warning/warning.ts
@@ -0,0 +1,11 @@
+export function bonWarning(message: string, errorType?: string): void {
+  if (message === undefined) {
+    throw new Error('Message is required to execute bonWarning function');
+  }
+  if (console !== undefined) {
+    const typeOutput = errorType ? ` - ${errorType}` : '';
+    const output = `BON-JSS${errorType}: ${message}`;
+    // tslint:disable-next-line:no-console
+    console.warn(output);
+  }
+}
