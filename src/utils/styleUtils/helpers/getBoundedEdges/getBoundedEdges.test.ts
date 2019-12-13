06e00faf06f9b6f4aac66b64046c505178f204c3
Roman Bova <roman.bova@wallester.eu>
2019-12-13 18:00:43 +0200

    Mock bonWarning function and rework getBoundedEdges testers to work with new logging function

--- src/utils/styleUtils/helpers/getBoundedEdges/getBoundedEdges.test.ts
+++ src/utils/styleUtils/helpers/getBoundedEdges/getBoundedEdges.test.ts
@@ -1,10 +1,9 @@
 import { BoundAttributeArray } from '../../models';
+import * as Warning from '../warning/warning';
 import { getBoundedEdges } from './getBoundedEdges';
 
 const keys: BoundAttributeArray = ['attr1', 'attr2', 'attr3', 'attr4'];
-
 const config = ['value1', 'value2', 'null', 'value4'];
-
 const response = {
   attr1: config[0],
   attr2: config[1],
@@ -16,19 +15,19 @@ it('works with correct attributes', () => {
 });
 
 it('works without attributes', () => {
-    // @ts-ignore
+  // @ts-ignore
   expect(getBoundedEdges()).toEqual({});
 });
 
 it('works with incorrect attributes', () => {
+  const spy = jest.spyOn(Warning, 'bonWarning').mockImplementation(() => null);
   const newKeys = keys.slice(0, 3) as BoundAttributeArray;
-  const newConfig = config.concat(config).join(' ');
+  const newConfig = config.concat(keys).join(' ');
   const newResponse = { ...response };
   delete newResponse.attr4;
-  try {
-    getBoundedEdges(keys, newConfig);
-  } catch (e) {
-    expect(e.message).toContain('Params error');
-  }
+
+  const emptyResponse = getBoundedEdges(keys, newConfig);
+  expect(Object.entries(emptyResponse).length).toBe(0);
   expect(getBoundedEdges(newKeys, config.join(' '))).toEqual(newResponse);
+  spy.mockRestore();
 });
