2b338ecf60f8044d5ee4f842e31d672e99fde878
Roman Bova <roman.bova@wallester.eu>
2019-12-13 22:34:59 +0200

    Add small fixes for styleUtils

--- src/utils/styleUtils/font/font.ts
+++ src/utils/styleUtils/font/font.ts
@@ -2,6 +2,9 @@ import { cleanMap, isParamUndefined } from '../helpers';
 import { cssAttribute } from '../models';
 
 // TODO: add fields as optional to improve usage experience
+// TODO: add lineHeight option
+// TODO: overload function to pass object
+
 export function setFont(fontSize: cssAttribute, color: string, fontWeight: cssAttribute, letterSpacing: cssAttribute) {
   if (isParamUndefined(fontSize) && isParamUndefined(color) && isParamUndefined(fontWeight) && isParamUndefined(letterSpacing)) {
     return null;
--- src/utils/styleUtils/helpers/boxEdges/boxEdges.test.ts
+++ src/utils/styleUtils/helpers/boxEdges/boxEdges.test.ts
@@ -4,67 +4,68 @@ import { boxEdges } from './boxEdges';
 
 let spy: jest.SpyInstance;
 let mockFunc: jest.MockedFunction<any>;
+describe('BoxEdges tests', () => {
+  beforeEach(() => {
+    spy = jest.spyOn(Warnings, 'bonWarning').mockImplementation(() => null);
+    mockFunc = Warnings.bonWarning;
+  });
 
-beforeEach(() => {
-  spy = jest.spyOn(Warnings, 'bonWarning').mockImplementation(() => null);
-  mockFunc = Warnings.bonWarning;
+  afterEach(() => {
+    spy.mockRestore();
+  });
+
+  const casesTitle = 'boxEdges cases';
+
+  const casesTester = (opts: typeof casesData[1]) => {
+    expect(boxEdges(opts.input)).toEqual(opts.output);
+    if (!opts.valid) {
+      expect(mockFunc).toBeCalledTimes(1);
+      const call = mockFunc.mock.calls[0];
+      expect(call[0].length).toBeGreaterThan(0);
+      expect(call[1]).toMatch('Box Edges Params');
+    } else {
+      expect(mockFunc).not.toBeCalled();
+    }
+  };
+
+  const casesData = [
+    {
+      name: 'For null should be null',
+      input: null,
+      output: null,
+      valid: true,
+    },
+    {
+      name: 'For 1 param',
+      input: '14px',
+      output: ['14px', '14px', '14px', '14px'],
+      valid: true,
+    },
+    {
+      name: 'For 2 params',
+      input: '14px 45px',
+      output: ['14px', '45px', '14px', '45px'],
+      valid: true,
+    },
+    {
+      name: 'For 3 params',
+      input: '14px 45px 2em',
+      output: ['14px', '45px', '2em', '45px'],
+      valid: true,
+    },
+    {
+      name: 'For 4 params',
+      input: '14px 45px 2em 5vw',
+      output: ['14px', '45px', '2em', '5vw'],
+      valid: true,
+    },
+    {
+      name: 'For 5 params',
+      input: '14px 45px 2em 5vw 54px',
+      output: null,
+      valid: false,
+    },
+  ];
+
+  cases(casesTitle, casesTester, casesData);
 });
-
-afterEach(() => {
-  spy.mockRestore();
-});
-
-const casesTitle = 'boxEdges cases';
-
-const casesTester = (opts: typeof casesData[1]) => {
-  expect(boxEdges(opts.input)).toEqual(opts.output);
-  if (!opts.valid) {
-    expect(mockFunc).toBeCalledTimes(1);
-    const call = mockFunc.mock.calls[0];
-    expect(call[0].length).toBeGreaterThan(0);
-    expect(call[1]).toMatch('Box Edges Params');
-  } else {
-    expect(mockFunc).not.toBeCalled();
-  }
-};
-
-const casesData = [
-  {
-    name: 'For null should be null',
-    input: null,
-    output: null,
-    valid: true,
-  },
-  {
-    name: 'For 1 param',
-    input: '14px',
-    output: ['14px', '14px', '14px', '14px'],
-    valid: true,
-  },
-  {
-    name: 'For 2 params',
-    input: '14px 45px',
-    output: ['14px', '45px', '14px', '45px'],
-    valid: true,
-  },
-  {
-    name: 'For 3 params',
-    input: '14px 45px 2em',
-    output: ['14px', '45px', '2em', '45px'],
-    valid: true,
-  },
-  {
-    name: 'For 4 params',
-    input: '14px 45px 2em 5vw',
-    output: ['14px', '45px', '2em', '5vw'],
-    valid: true,
-  },
-  {
-    name: 'For 5 params',
-    input: '14px 45px 2em 5vw 54px',
-    output: null,
-    valid: false,
-  },
-];
-
-cases(casesTitle, casesTester, casesData);
